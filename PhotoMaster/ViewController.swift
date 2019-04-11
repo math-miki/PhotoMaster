7462eca3cf62df6d398e03ee28351837db01d916
Miki Takahashi <math.micky.dtl@gmail.com>
2019-04-12 02:44:08 +0900

    Initial Commit

---
+++ PhotoMaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoMaster
+//
+//  Created by Miki Takahashi on 2019/04/12.
+//  Copyright © 2019 MikiTakahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
